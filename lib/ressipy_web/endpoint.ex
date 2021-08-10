df65a4837a4eff8c55aa1512aa0c396b11e2764a
Dennis Beatty <dennis@dcbeatty.com>
2021-08-09 19:26:25 -0600

    add apple site association file

--- lib/ressipy_web/endpoint.ex
+++ lib/ressipy_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule RessipyWeb.Endpoint do
     at: "/",
     from: :ressipy,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js .well-known favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
