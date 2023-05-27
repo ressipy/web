edaf6b1e8065277be4dbe81ede5d7444a4a811f9
Dennis Beatty <dennis@dcbeatty.com>
2023-05-27 20:33:42 +0100

    remove requirement on redis

--- config/runtime.exs
+++ config/runtime.exs
@@ -43,13 +43,7 @@ if config_env() == :prod do
     adapter: Swoosh.Adapters.Sendgrid,
     api_key: sendgrid_api_key
 
-  redis_url =
-    System.get_env("FLY_REDIS_CACHE_URL") ||
-      raise """
-      environment variable FLY_REDIS_CACHE_URL is missing.
-      For example: redis://USER:PASS@HOST:PORT
-      """
-
+  redis_url = System.get_env("FLY_REDIS_CACHE_URL")
   hammer_redis_opts = [expiry_ms: 60_000 * 60 * 2, redis_url: redis_url]
   config :hammer, backend: {Hammer.Backend.Redis, hammer_redis_opts}
 end
